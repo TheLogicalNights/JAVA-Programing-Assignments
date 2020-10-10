5320c16c8b306fa3ad1759913c7e532baeae9f17
Swapnil Ramesh Adhav <69031236+TheLogicalNights@users.noreply.github.com>
2020-10-10 17:08:23 +0530

    Add files via upload

---
+++ Assignment_5/Program_3/Main.java
@@ -0,0 +1,45 @@
+/*
+    Problem Statement : Write Java program which accept number of rows and number of columns
+                        from user and display below pattern.
+                
+                        Input : iRow = 3 iCol = 5
+                        Output : A A A A A
+                                 B B B B B
+                                 C C C C C
+*/
+
+import java.util.*;
+
+class Pattern
+{
+    void Display(int iRow,int iCol)
+    {
+        char ch = 'A';
+        for(int i=0;i<iRow;i++)
+        {
+            for(int j=0;j<iCol;j++)
+            {
+                System.out.print(ch+"\t");
+            }
+            ch++;
+            System.out.println();
+        }
+    }
+}
+
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner cin = new Scanner(System.in);
+        Pattern pobj = new Pattern();
+
+        System.out.println("Enter no of rows");
+        int iRow = cin.nextInt();
+
+        System.out.println("Enter no of columns");
+        int iCol = cin.nextInt();
+
+        pobj.Display(iRow,iCol);
+    }
+}
