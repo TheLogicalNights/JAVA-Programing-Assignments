38714d2b8ccba22eb41b8d5926d902aeb05c6beb
Swapnil Ramesh Adhav <69031236+TheLogicalNights@users.noreply.github.com>
2020-10-20 22:08:38 +0530

    Add files via upload

---
+++ Assignment_12/Program_3/Main.java
@@ -0,0 +1,52 @@
+/*
+    Problem Statement : Write Java program which accept String from user and display
+                        below pattern.
+
+                        Input : Hello
+
+                        Output : H * * * *
+                                 H e * * *
+                                 H e l * *
+                                 H e l l *
+                                 H e l l o
+*/
+
+import java.util.*;
+
+class Pattern
+{
+    void Display(String str)
+    {
+        char arr[] = str.toCharArray();
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr.length;j++)
+            {
+                if(j<=i)
+                {
+                    System.out.printf(arr[j]+"\t");
+                }
+                else
+                {
+                    System.out.printf("*\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner cin = new Scanner(System.in);
+        Pattern pobj = new Pattern();
+
+        System.out.println("Enter a string");
+        String str = cin.nextLine();
+
+        pobj.Display(str);
+    }
+}
